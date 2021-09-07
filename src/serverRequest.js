1397853aa7305af5e1c6f45aafd78451cbe1a090
Ivan Kolesnev <i.kolesnev@softteco.com>
2021-09-07 20:56:01 +0300

    create in html database info and create get request

--- src/serverRequest.js
+++ src/serverRequest.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sendButton = document.getElementById("send"); // button send
+  const openButton = document.getElementById("open") // button open
+  const infoDatabase = document.getElementById("info") // info database
+  const request = new XMLHttpRequest();
   const regexp = /[^(\d+(.\d+)?)]/g;
+
   sendButton.addEventListener("click", function () {
     let payload =
-      "name=" + encodeURIComponent(document.getElementById("value").value);
+        "name=" + encodeURIComponent(document.getElementById("value").value);
 
-    const request = new XMLHttpRequest();
     request.open("POST", "http://34.125.1.13:8080/cgi-bin/script.cgi", true);
 
     request.addEventListener("readystatechange", function () {
@@ -16,9 +19,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     request.setRequestHeader(
-      "Content-Type",
-      "application/x-www-form-urlencoded"
+        "Content-Type",
+        "application/x-www-form-urlencoded"
     );
     request.send(payload);
   });
+
+  openButton.addEventListener("click", function () {
+    // проверить работает ли этот запрос на получение из базы данных текста
+    request.open("GET", "http://34.125.1.13:8080/cgi-bin/script.cgi");
+    request.send();
+
+    request.onload = function () {
+      console.log(this.responseText);
+      infoDatabase.innerHTML = this.responseText;
+      openButton.disabled = true;
+    };
+  })
 });
