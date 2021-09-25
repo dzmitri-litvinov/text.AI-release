a475021c70c0838f3026efc46a28d815339485af
dzmitri-litvinov <dzmitri.litvinov@gmail.com>
2021-09-25 17:17:55 +0300

    Add changes

--- text.cpp
+++ text.cpp
@@ -76,19 +76,9 @@ int main()
     string name;
 
     cout << "Content-type:text/html\r\n\r\n";
-    cout << "<html>\n";
-    cout << "<head>\n";
-    cout << "<title>Ggi Server</title>\n";
-    cout << "</head>\n";
-    cout << "<body>\n";
-    cout << "<p>";
 
     cout << getDB() << "\n";
     
-    cout << "</p>\n";
-    cout << "</body>\n";
-    cout << "</html>\n";
-
     return 0;
 }
 
